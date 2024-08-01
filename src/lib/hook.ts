60249822a414784b80eb7e406b2dcf6cacb0f783
Kiran Badave <kiranb4352@gmail.com>
2024-08-01 20:44:22 +0530

    Position fixed

--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -56,6 +56,7 @@ export function useHoveredParagraphCoordinate(
 ): HoveredElementInfo | null {
   
   const hoveredInfoArray:HoveredElementInfo[] = [];
+  console.log(parsedElements);
   for(const element of parsedElements){
     
     const boundBox = getElementBounds(element);
